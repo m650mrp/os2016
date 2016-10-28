f4ae4d3238150330af5ed4eab145db66f3e47327
root <root@localhost.localdomain>
2016-10-28 18:37:11 +0000

    unifile upload

--- print.c
+++ print.c
@@ -1,8 +1,14 @@
 #include "print.h"
+#include "inttypes.h"
 
 void print_line(int64_t number, char *string)
 {
 	(void) number;
 	(void) string;
 	// Add code here.
+	int64_t x;
+	uint32_t y;
+	printf("x: %"PRIdN", y: %"PRId32"\n", x, y);
+
+
 }
