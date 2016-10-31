37fdcaae04b28eb763b093d9e49943c4ca32b973
root <root@localhost.localdomain>
2016-10-31 05:55:20 +0000

    new folder

---
+++ p1/print.c
@@ -0,0 +1,14 @@
+#include "print.h"
+#include <stdio.h>
+#include <inttypes.h>
+
+void print_line(int64_t number, char *string)
+{
+	(void) number;
+	(void) string;
+	// Add code here.
+	int64_t i = number;
+	printf("%"PRId64" ""%s""\n", i, string);
+
+
+}
